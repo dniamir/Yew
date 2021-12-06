5cb37a1cd3d00e417e96f8d85a6fc0140194dfa0
Darien <darien.niamir@gmail.com>
2021-12-06 01:00:11 -0800

    Include code for microcontroller to read GPS using updated chip_systems_cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,7 @@
 # include "Wire.h"
 # include "bmm150.h"
 # include "icm20649.h"
-
-/* LED Blink, Teensyduino Tutorial #1
-   http://www.pjrc.com/teensy/tutorial.html
-
-   This example code is in the public domain.
-*/
+# include "CDPA1616S.h"
 
 // Teensy 2.0 has the LED on pin 11
 // Teensy++ 2.0 has the LED on pin 6
@@ -18,22 +13,37 @@ bool imu_check = false;
 
 BMM150 bmm = BMM150();
 ICM20649 imu = ICM20649();
+CDPA1616S gps = CDPA1616S();
+
+uint32_t gps_reading_ms = millis();
+uint32_t led_off_ms = millis();
+uint32_t led_on_ms = millis();
+uint32_t mag_reading_ms = millis();
+uint32_t imu_reading_ms = millis();
 
 // the setup() method runs once, when the sketch starts
 
 void setup() {
-  // initialize the digital pin as an output.
-  while (!Serial) ;
+  Serial.begin(115200);
 
+  while (!Serial);
+
+  // initialize the digital pin as an output.
   pinMode(ledPin, OUTPUT);
   Serial.println("Test 1");
 
   Wire.begin();
 
+  // Set the TX and RX pins to the alternate TX/RX 1 pins
+  Serial1.setTX(26);
+  Serial1.setRX(27);
+
+  // Initialize BMM and IMUs
   bmm_check = bmm.initialize();
   imu_check = imu.initialize();
 
-
+  // Initialize GPS
+  gps.initialize();
 
 }
 
@@ -41,19 +51,43 @@ void setup() {
 // as long as the board has power
 
 void loop() {
-    digitalWrite(ledPin, HIGH);   // set the LED on
-    delay(200);                  // wait for a second
-    digitalWrite(ledPin, LOW);    // set the LED off
-    delay(200);                  // wait for a second
+
+  // LED On
+  if (millis() - led_off_ms > 200){
+    digitalWrite(ledPin, HIGH); 
+    led_on_ms = millis();
+    led_off_ms = millis() + 1;
+  }
+
+  // LED Off
+  if (millis() - led_on_ms > 200){
+    digitalWrite(ledPin, LOW); 
+    led_off_ms = millis();
+    led_on_ms = millis() + 1;
+  }                 
+
+  // BMI Check
+  if (millis() - mag_reading_ms > 1000){
     Serial.println("BMM Check");
     Serial.println(bmm_check);
-
     Serial.println();
+    mag_reading_ms = millis();
+  }
 
+  // IMU Check
+  if (millis() - imu_reading_ms > 1000){
     Serial.println("IMU Check");
     Serial.println(imu_check);
+    Serial.println();
+    imu_reading_ms = millis();
+  }
 
-    
-    
-    // cout << "x is equal to " << x;
+  // GPS Check
+  gps.readGPS();
+  if (millis() - gps_reading_ms > 2000) {
+    Serial.println("GPS Check");
+    gps_reading_ms = millis(); // reset the timer
+    gps.printBlock();
+    Serial.println();
+  }
 }
